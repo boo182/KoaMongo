2e9f5b7ab29b574b317e6016aa6aae9076a04b14
boo182 <lucien.ladurie@gmail.com>
2017-12-20 12:40:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -47,23 +47,18 @@ promise.then(function(db) {
   console.log('logged on');
 });
 
-
-// MongoDB连接成功后回调，这里仅输出一行日志
 mongoose.connection.on('connected', function () {
     console.log('Mongoose default connection open to ' + connString);
 });
 
-// MongoDB连接出错后回调，这里仅输出一行日志
 mongoose.connection.on('error',function (err) {
     console.log('Mongoose default connection error: ' + err);
 });
 
-// MongoDB连接断开后回调，这里仅输出一行日志
 mongoose.connection.on('disconnected', function () {
     console.log('Mongoose default connection disconnected');
 });
 
-// 当前进程退出之前关闭MongoDB连接
 process.on('SIGINT', function() {
     mongoose.connection.close(function () {
         console.log('Mongoose default connection closed through app termination');
